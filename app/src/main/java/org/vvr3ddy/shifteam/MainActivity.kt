89de3f60d76c3cf35aedcf797cf0f0fbf1b1027b
Vishnuvardhan Reddy <vvr-rockstar@bmsit.in>
2024-08-07 19:08:14 +0100

    ProfileScreen: Add Functionality Placeholders
    - Introduce Buttons for Logout and Edit Profile
    - Add a Placeholder for Support Contact

--- app/src/main/java/org/vvr3ddy/shifteam/MainActivity.kt
+++ app/src/main/java/org/vvr3ddy/shifteam/MainActivity.kt
@@ -5,23 +5,33 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import androidx.navigation.compose.currentBackStackEntryAsState
-import androidx.navigation.compose.rememberNavController
-import org.vvr3ddy.shifteam.ui.theme.ShifTeamTheme
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.outlined.Home
 import androidx.compose.material.icons.outlined.Person
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import org.vvr3ddy.shifteam.ui.screens.HomePageScreen
 import org.vvr3ddy.shifteam.ui.screens.ProfileScreen
+import org.vvr3ddy.shifteam.ui.theme.ShifTeamTheme
 
 class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalMaterial3Api::class)
--- app/src/main/java/org/vvr3ddy/shifteam/ui/screens/ProfileScreen.kt
+++ app/src/main/java/org/vvr3ddy/shifteam/ui/screens/ProfileScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,16 +12,21 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.twotone.AccountCircle
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.buildAnnotatedString
@@ -109,15 +115,68 @@ class ProfileScreen private constructor() {
                     }
                 }
             }
+            HorizontalDivider(
+                modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp)
+            )
             Card(
-                modifier = Modifier.fillMaxWidth()
+                modifier = Modifier
+                    .fillMaxWidth()
                     // Compose seems to calculate the Max height
                     // as available max height - occupied height
-                    // Hence, select 25% of the full height and not 25% of remaining height
-                    .fillMaxHeight(0.25f/0.75f)
-                    .padding(16.dp)
+                    // Hence, select 15% of the full height and not 12.5% of remaining height
+                    .fillMaxHeight(0.125f / 0.75f)
+                    .padding(16.dp),
+                colors = CardDefaults.cardColors(
+                    containerColor = Color.Transparent,
+                ),
             ){
-
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceAround,
+                    verticalAlignment = Alignment.CenterVertically
+                ){
+                    ElevatedButton(
+                        onClick = { /*TODO*/ },
+                        shape = RoundedCornerShape(12.dp),
+                        colors = ButtonDefaults.elevatedButtonColors(
+                            containerColor = MaterialTheme.colorScheme.primaryContainer,
+                            contentColor = MaterialTheme.colorScheme.onPrimaryContainer
+                        ),
+                        modifier = Modifier
+                            .weight(1f)
+                            .fillMaxHeight()
+                    ) {
+                        Text(text = "Edit Profile")
+                    }
+                    Spacer(
+                        modifier = Modifier.weight(0.25f)
+                    )
+                    ElevatedButton(
+                        onClick = { /*TODO*/ },
+                        shape = RoundedCornerShape(12.dp),
+                        colors = ButtonDefaults.elevatedButtonColors(
+                            containerColor = MaterialTheme.colorScheme.errorContainer,
+                            contentColor = MaterialTheme.colorScheme.onErrorContainer
+                        ),
+                        modifier = Modifier
+                            .weight(1f)
+                            .fillMaxHeight(),
+                    ) {
+                        Text(text = "Logout")
+                    }
+                }
+            }
+            // Placeholder card for Support
+            OutlinedCard(
+                modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxWidth()
+                    .fillMaxHeight(1f)
+            ){
+                Text(
+                    text = "Support",
+                    modifier = Modifier.padding(16.dp)
+                )
             }
         }
     }
